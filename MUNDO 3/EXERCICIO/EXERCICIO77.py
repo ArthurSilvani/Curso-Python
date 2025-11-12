a18866ed1de1b563345398bbf75c1fbcb3ec1988
ArthurSilvani <arthurpsilvani@gmail.com>
2025-11-11 21:30:02 -0300

    teste

--- MUNDO 3/EXERCICIO/EXERCICIO77.py
+++ MUNDO 3/EXERCICIO/EXERCICIO77.py
@@ -1,7 +1,7 @@
 #CRIE UM PROGRAMA QUE TENHA UMA TUPLA COM VÁRIAS PALAVRAS (NÃO USAR ACENTOS).
 #dEPOIS DISSO, VOCÊ DEVE MOSTRAR PARA CADA PALAVRA AS SUAS VOGAIS.     
 
-palavras = ('programar','estudar','pensar','comprir','jogar','furia')
+palavras = ('programar','estudar','pensar','comprir','jogar','furia', 'agua')
 
 for p in palavras:  
     print(f'\nNa palavra {p.upper()} temos as vogais : ',end='') #AQUI EU QUEBREI E AJUSTEI EM UMA LINHA COM O END
